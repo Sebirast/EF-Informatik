23803c18c82e99459ba7ad1483f591a5bd07a570
sebirast <sebastian.rast@stud.schulegl.ch>
2021-09-08 08:24:07 +0200

    added dezInBin method

--- Basics/src/com/company/Main.java
+++ Basics/src/com/company/Main.java
@@ -1,10 +1,33 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
-    public static int[] dezInBin(int input, ){
-        while()
+    public static List<Integer> dezInBin(int input, int devisor){
+        int a = input;
+        int result;
+        List<Integer> resultArray = new ArrayList<>();
+
+        if(devisor > input){
+            input = devisor;
+            devisor = a;
+        }
+
+        do{
+            result = input % devisor;
+            resultArray.add(result);
+            input = input / devisor;
+
+        }while(input != 0);
+
+        Collections.reverse(resultArray);
+
+        return resultArray;
     }
 
     public static void main(String[] args) {
+        System.out.println(dezInBin(7, 2));
     }
 }
